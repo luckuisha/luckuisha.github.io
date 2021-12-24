ae2188b147729cadad7c9dc0af2b8569bbdbbc38
luckykbkim@gmail.com <32375937+luckuisha@users.noreply.github.com>
2021-12-24 15:10:04 -0500

    try to match bg color but failed

--- app.js
+++ app.js
@@ -1,51 +1,57 @@
 
 ///////// animation when entering site ////////////////
 
-let intro = document.querySelector('.intro');
-let intro_logo = document.querySelector('.intro-logo');
-let intro_name_header = document.querySelector('.intro-name-header');
-let intro_name = document.querySelectorAll('.intro-name');
-let intro_description_header = document.querySelector('.intro-description-header');
-let intro_description = document.querySelector('.intro-description');
-let intro_wip_header = document.querySelector('.intro-wip-header');
-let intro_wip = document.querySelector('.intro-wip');
+let myStorage = window.localStorage;
+myStorage.setItem('intro-played', false);
+
+const intro = document.querySelector('.intro');
+const intro_logo = document.querySelector('.intro-logo');
+const intro_name = document.querySelectorAll('.intro-name');
+const intro_description = document.querySelector('.intro-description');
+const intro_wip = document.querySelector('.intro-wip');
 
 
 window.addEventListener('DOMContentLoaded', ()=>{
-    setTimeout(()=> {
-        //fade in
-        setTimeout(()=> {
-            intro_logo.classList.add('active');
-            intro_description.classList.add('active');
-            intro_wip.classList.add('active');
-            intro_name.forEach((span, idx) => {
-                setTimeout(()=>{
-                    span.classList.add('active');
-                }, (idx + 1) * 400);
-            });
-        }, 100);
-        
-        //fade out
-        setTimeout(()=> {
-            intro_logo.classList.remove('active');
-            intro_logo.classList.add('fade');
-            intro_description.classList.remove('active');
-            intro_description.classList.add('fade');
-            intro_wip.classList.remove('active');
-            intro_wip.classList.add('fade');
-            intro_name.forEach((span, idx) => {
-                setTimeout(()=>{
-                    span.classList.remove('active');
-                    span.classList.add('fade');
-                }, (idx + 1) * 400);
-            });
-        }, 3000);
 
-        //remove overall div
+    console.log(myStorage.getItem('intro-played'))
+    if (myStorage.getItem('intro-played') == 'false'){
         setTimeout(()=> {
-            intro.style.top = '-100vh';
-        }, 5000);
+            //fade in
+            setTimeout(()=> {
+                intro_logo.classList.add('active');
+                intro_description.classList.add('active');
+                intro_wip.classList.add('active');
+                intro_name.forEach((span, idx) => {
+                    setTimeout(()=>{
+                        span.classList.add('active');
+                    }, (idx + 1) * 400);
+                });
+            }, 100);
+            
+            //fade out
+            setTimeout(()=> {
+                intro_logo.classList.remove('active');
+                intro_logo.classList.add('fade');
+                intro_description.classList.remove('active');
+                intro_description.classList.add('fade');
+                intro_wip.classList.remove('active');
+                intro_wip.classList.add('fade');
+                intro_name.forEach((span, idx) => {
+                    setTimeout(()=>{
+                        span.classList.remove('active');
+                        span.classList.add('fade');
+                    }, (idx + 1) * 400);
+                });
+            }, 3000);
 
-    });
+            //remove overall div
+            setTimeout(()=> {
+                intro.style.top = '-100vh';
+            }, 5000);
+
+            myStorage.setItem('intro-played', true);
+
+        });
+    }
 
 });
